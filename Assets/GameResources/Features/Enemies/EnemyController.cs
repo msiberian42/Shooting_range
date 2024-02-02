acda4649e6b2a4f11d80e0d430e44152d787d2ef
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-02 20:10:00 +0300

    Написал скрипты для врага и цели

---
+++ Assets/GameResources/Features/Enemies/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿namespace ShootingRange.Features.Enemies
+{
+    using UnityEngine;
+    using UnityEngine.AI;
+
+    /// <summary>
+    /// Контроллер врага
+    /// </summary>
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyController : MonoBehaviour
+    {
+        protected NavMeshAgent agent = default;
+        protected EnemyTargetsController targetsController = default;
+        protected Transform target = default;
+
+        protected virtual void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+
+            targetsController = FindAnyObjectByType<EnemyTargetsController>();
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (targetsController != null)
+            {
+                target = targetsController.GetRandomTarget();
+                agent.SetDestination(target.position);
+            }
+        }
+    }
+}
---
+++ Assets/GameResources/Features/Enemies/EnemyTargetsController.cs
@@ -0,0 +1,20 @@
+﻿namespace ShootingRange.Features.Enemies
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Контроллер целей для врагов
+    /// </summary>
+    public class EnemyTargetsController : MonoBehaviour
+    {
+        [SerializeField]
+        protected List<Transform> targets = new List<Transform>();
+
+        /// <summary>
+        /// Возвращает случайную цель
+        /// </summary>
+        public virtual Transform GetRandomTarget() => 
+            targets[Random.Range(0, targets.Count)];
+    }
+}
